6b696d559050c53e0ffa9dc4511fbef0ae61cd84
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2025-01-09 12:20:29 +0600

    A boilerplate code for backend

--- src/utils/generateToken.ts
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generateToken = (user_id: string, role: string): string => {
-  return jwt.sign({ user_id, role }, process.env.JWT_SECRET as string, { expiresIn: '1d' });
-};
-
-export default generateToken;
